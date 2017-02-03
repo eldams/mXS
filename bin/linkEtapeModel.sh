9416293b4e8c84690ca94d9fb5f16ab74962154f
eldams <damien.nouvel@inalco.fr>
2017-02-03 12:09:25 +0100

    Intialisation liaison

---
+++ bin/linkEtapeModel.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+[[ -d $MXS_PATH ]] || export MXS_PATH=`pwd`
+[[ -d $TREETAGGER_PATH ]] || source $MXS_PATH/bin/conf_machineExample.sh
+source $MXS_PATH/bin/conf_EtapeModel.sh
+$MXS_PATH/bin/tagSciKit.sh | nel.py
---
+++ bin/nel.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Trouver les entités en sortie de bin/tagEtapeModel qui ont un lien dans dicos/links.lst
+# Si lien trouvé ajouter un attribut link à la balise
+# <pers>Nicolas Sarkozy</pers>
+# <pers link="https://fr.wikipedia.org/wiki/Nicolas_Sarkozy">Nicolas Sarkozy</pers>
