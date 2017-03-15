4dff951c622dd81a62f5a788914849e0d8c42891
eldams <damien.nouvel@inalco.fr>
2017-03-15 17:48:34 +0100

    Bug with URLs

--- bin/neTagsToCorpus.sh
+++ bin/neTagsToCorpus.sh
@@ -5,18 +5,21 @@ tr '\n' ' ' |
 sed -E 's| +| |g' |
 # Recover new items
 sed -E 's|NEW/(NE/</?NE)|\1m|g' |
-# Recover original data format
+# Recover tokens
+sed -E 's#( |^)[^ <]*[^<]/([^/ ]*)#\1\2#g' |
+# Remove tags
 sed -E 's/(<_n|n_>)//g' |
 sed -e 's|<_b|\'$'\n|g' |
 sed -E 's|<_t|<|g' |
 sed -E 's|t_>|>|g' |
 sed -E 's|<_c|[|g' |
 sed -E 's|c_>|]|g' |
-sed -E 's#( |^)[^ ]*[^<]/([^/ ]*)#\1\2#g' |
+# Replace special chars
 sed -E 's|MXS_SLASH|/|g' |
 sed -E 's|MXS_PLUS|+|g' |
 sed -E 's|MXS_DASH|-|g' |
 sed -E 's|MXS_SPACE| |g' |
+# Other replacements
 sed -E 's|((<[^>]*> +)*)(<_s([^>]*)s_>)|\3 \1|g' |
 sed -E 's#( |^)[^ ]+ +((NE/[^ ]* +)*)<_s([^>]*)s_>#\1\4 \2#g' |
 sed -E "s| ' |' |g" |
